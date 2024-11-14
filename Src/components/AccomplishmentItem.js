cf3cde00d452a739961b336cedb902e1b2abc153
dariuskemei <dariuskimutai76@gmail.com>
2024-11-14 04:16:26 +0300

    first commit

---
+++ Src/components/AccomplishmentItem.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+function AccomplishmentItem({accomplishment, onDeleteClick});
+  const{id, name, complited} = accomplishment;
+
+  const handleDelete = () =>{
+    let type ="accomplishments"
+    onDeleteClick(id, type)
+  }
+
+  return(
+    <div className="card">
+        <button onClick={handleDelete} classname="delete-button">X</button>
+        
+  )
