eb2fafd805a32c6757c85a859fcbbffb92349510
dariuskemei <dariuskimutai76@gmail.com>
2024-11-14 10:25:56 +0300

    first commit

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Home(){
+    return(
+        <div id="home-container">
+            <h3>Vision+Board</h3>
+            <p>Just a simple goal board app created with React...</p>
+            {'\n'}
+            <p id="author">By Darius Kemei -- <a href="">LinkedIn</a></p>
+            
+        </div>
+    );
+}
+
+export default Home;
