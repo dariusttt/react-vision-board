33fe7fc0056f97bc1657e9ed66ce4be9c5335cdf
dariuskemei <dariuskimutai76@gmail.com>
2024-11-15 02:11:51 +0300

    first commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { Route, Switch } from "react-router-dom";
+
+import NavBar from "./components/NavBar";
+import Goals from "./components/Goals";
+import Accomplishments from "./components/Accomplishments";
+import Home from "./components/Home";
+import Form from "./components/Form";
 
 function App() {
+  const [goals, setGoals] = useState([]);
+  const [accomplishments, setAccomplishments] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:/goals`)
+      .then(res => res.json())
+      .then(setGoals);
+  }, []);
+
+  useEffect(() => {
+    fetch(`http://localhost:/accomplishments`)
+      .then(res => res.json())
+      .then(setAccomplishments);
+  }, []);
+
+  const onUpdateGoal = (updatedGoal) => {
+    const updatedGoals = goals.map(originalGoal => {
+      if(originalGoal.id === updatedGoal.id){
+        return updatedGoal;
+      } else{
+        return originalGoal;
+      }
+    });
+    console.log(updatedGoals);
+  }
+
+  const onFormSubmit = (newData, type) => {
+      type === 'goals' ? setGoals(goals => [...goals, newData]) : setAccomplishments(accomplishments => [...accomplishments, newData])
+  }
+
+  const onDeleteClick = (id, type) => {
+    fetch(`http://localhost:/${type}/${id}`, {
+      method: 'DELETE'
+    })
+    
+    //Use filter to setGoal or setAccomplishments with new items
+      if(type === 'goals'){
+        setGoals(ogGoals => ogGoals.filter(ogGoal => {
+          return ogGoal.id === id ? null : ogGoal;
+        }))
+      } else{
+        setAccomplishments(ogAccomplishments => ogAccomplishments.filter(ogAccomplishment => {
+          return ogAccomplishment.id === id ? null : ogAccomplishment;
+        }))
+      }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavBar />
+      <Form onFormSubmit={onFormSubmit}/>
+      <Switch>
+      <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/goals">
+          <Goals goals={goals} onUpdateGoal={onUpdateGoal} onDeleteClick={onDeleteClick}/>
+        </Route>
+        <Route path="/accomplishments">
+          <Accomplishments accomplishments={accomplishments} onDeleteClick={onDeleteClick}/>
+        </Route>
+      </Switch>
     </div>
   );
 }
